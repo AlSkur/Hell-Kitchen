3711cab154666f69241447d91d36e70bfac1d097
AlSkur <164603354+AlSkur@users.noreply.github.com>
2024-04-25 15:53:56 +0700

    main commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,34 +6,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
 
     private bool isWalking;
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        Vector3 moveDir = new Vector3(inputVector.x, 0 , inputVector.y);
-        inputVector = inputVector.normalized;
-
+        Vector2 inputVector = gameInput.GetInputVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         isWalking = moveDir != Vector3.zero;
 
-
+        
         float rotateSpeed = 10f;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
